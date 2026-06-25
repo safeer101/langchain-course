2508037fa34cdec6649212cdad968e1ca47523c2
safeer101 <mohdsafeerullahkhan@gmail.com>
2026-06-25 11:57:14 +0100

    Environment Setup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-
+# Safeer
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,7 +6,6 @@ load_dotenv()
 
 def main():
     print("Hello from langchain-course!")
-    print(os.environ.get("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
